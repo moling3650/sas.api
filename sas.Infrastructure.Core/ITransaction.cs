4fd28caafd8e48041a5225f432a060dea6750c7f
moling <365024424@qq.com>
2020-03-07 14:36:51 +0800

    Add the interfaces for sas.Infrastructure.Core

---
+++ sas.Infrastructure.Core/ITransaction.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+using System.Threading.Tasks;
+
+namespace sas.Infrastructure.Core
+{
+    public interface ITransaction
+    {
+        IDbContextTransaction GetCurrentTransaction();
+
+        bool HasActiveTransaction { get; }
+
+        Task<IDbContextTransaction> BeginTransactionAsync();
+
+        Task CommitTransactionAsync(IDbContextTransaction transaction);
+
+        void RollbackTransaction();
+    }
+}

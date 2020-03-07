a77a78fb23442a63420f2e3ebc8306b0f7c7b907
moling <365024424@qq.com>
2020-03-07 16:38:10 +0800

    Add EntityConfigurations for sas.Infrastructure

--- sas.Infrastructure/DomainContext.cs
+++ sas.Infrastructure/DomainContext.cs
@@ -1,7 +1,9 @@
 ﻿using DotNetCore.CAP;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using sas.Domain.CustomerAggregate;
 using sas.Infrastructure.Core;
+using sas.Infrastructure.EntityConfigurations;
 
 namespace sas.Infrastructure
 {
@@ -9,6 +11,16 @@ namespace sas.Infrastructure
     {
         public DomainContext(DbContextOptions options, IMediator mediator, ICapPublisher capBus) : base(options, mediator, capBus)
         {
+        }
+
+        public DbSet<Customer> Orders { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            #region 注册领域模型与数据库的映射关系
+            modelBuilder.ApplyConfiguration(new CustomerEntityTypeConfiguration());
+            #endregion
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
---
+++ sas.Infrastructure/EntityConfigurations/CustomerEntityTypeConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using sas.Domain.CustomerAggregate;
+
+namespace sas.Infrastructure.EntityConfigurations
+{
+    class CustomerEntityTypeConfiguration : IEntityTypeConfiguration<Customer>
+    {
+        public void Configure(EntityTypeBuilder<Customer> builder)
+        {
+            builder.HasKey(p => p.Id);
+            builder.ToTable("customer");
+            builder.Property(p => p.PhoneNo).HasMaxLength(20);
+            builder.OwnsOne(o => o.Address, a =>
+            {
+                a.WithOwner();
+                a.Property(p => p.Province).HasMaxLength(20);
+                a.Property(p => p.City).HasMaxLength(20);
+            });
+        }
+    }
+}

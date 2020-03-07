9d3889f141538833e9a7622a4288c701f7e3e6d5
moling <365024424@qq.com>
2020-03-07 15:06:19 +0800

    Add the Repository class for sas.Infrastructure.Core

---
+++ sas.Infrastructure.Core/Repository.cs
@@ -0,0 +1,50 @@
+﻿using sas.Domain;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace sas.Infrastructure.Core
+{
+    public abstract class Repository<TEntity, TDbContext> : IRepository<TEntity> where TEntity : Entity, IAggregateRoot where TDbContext : EFContext
+    {
+        protected virtual TDbContext DbContext { get; set; }
+
+        public Repository(TDbContext context)
+        {
+            this.DbContext = context;
+        }
+
+        public virtual IUnitOfWork UnitOfWork => DbContext;
+
+        public virtual TEntity Add(TEntity entity)
+        {
+            return DbContext.Add(entity).Entity;
+        }
+
+        public virtual Task<TEntity> AddAsync(TEntity entity, CancellationToken cancellationToken = default)
+        {
+            return Task.FromResult(Add(entity));
+        }
+
+        public virtual TEntity Update(TEntity entity)
+        {
+            return DbContext.Update(entity).Entity;
+        }
+
+        public virtual Task<TEntity> UpdateAsync(TEntity entity, CancellationToken cancellationToken = default)
+        {
+            return Task.FromResult(Update(entity));
+        }
+
+        public virtual bool Remove(Entity entity)
+        {
+            DbContext.Remove(entity);
+            return true;
+        }
+
+        public virtual Task<bool> RemoveAsync(Entity entity)
+        {
+            return Task.FromResult(Remove(entity));
+        }
+    }
+
+}

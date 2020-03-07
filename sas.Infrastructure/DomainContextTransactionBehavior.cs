03477d4351a257efcf82dd8efacffb79e12e351b
moling <365024424@qq.com>
2020-03-07 16:56:23 +0800

    Add DomainContextTransactionBehavior for sas.Infrastructure

---
+++ sas.Infrastructure/DomainContextTransactionBehavior.cs
@@ -0,0 +1,13 @@
+﻿using DotNetCore.CAP;
+using Microsoft.Extensions.Logging;
+using sas.Infrastructure.Core.Behaviors;
+
+namespace sas.Infrastructure
+{
+    public class DomainContextTransactionBehavior<TRequest, TResponse> : TransactionBehavior<DomainContext, TRequest, TResponse>
+    {
+        public DomainContextTransactionBehavior(DomainContext dbContext, ICapPublisher capBus, ILogger<DomainContextTransactionBehavior<TRequest, TResponse>> logger) : base(dbContext, capBus, logger)
+        {
+        }
+    }
+}

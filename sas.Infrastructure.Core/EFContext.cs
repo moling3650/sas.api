dc7181ed9939179283b792e841ff2dc9ea9fb0ac
moling <365024424@qq.com>
2020-03-07 15:04:14 +0800

    Add the EFContext class for sas.Infrastructure.Core

---
+++ sas.Infrastructure.Core/EFContext.cs
@@ -0,0 +1,85 @@
+﻿using DotNetCore.CAP;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using sas.Infrastructure.Core.Extensions;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace sas.Infrastructure.Core
+{
+    public class EFContext : DbContext, IUnitOfWork, ITransaction
+    {
+        protected IMediator _mediator;
+        protected ICapPublisher _capBus;
+
+        public EFContext(DbContextOptions options, IMediator mediator, ICapPublisher capBus) : base(options)
+        {
+            _mediator = mediator;
+            _capBus = capBus;
+        }
+
+        #region IUnitOfWork
+        public async Task<bool> SaveEntitiesAsync(CancellationToken cancellationToken = default)
+        {
+            await base.SaveChangesAsync(cancellationToken);
+            await _mediator.DispatchDomainEventsAsync(this);
+            return true;
+        }
+        #endregion
+
+        #region ITransaction
+        private IDbContextTransaction _currentTransaction;
+        public IDbContextTransaction GetCurrentTransaction() => _currentTransaction;
+        public bool HasActiveTransaction => _currentTransaction != null;
+        public Task<IDbContextTransaction> BeginTransactionAsync()
+        {
+            if (_currentTransaction != null) return null;
+            _currentTransaction = Database.BeginTransaction(_capBus, autoCommit: false);
+            return Task.FromResult(_currentTransaction);
+        }
+
+        public async Task CommitTransactionAsync(IDbContextTransaction transaction)
+        {
+            if (transaction == null) throw new ArgumentNullException(nameof(transaction));
+            if (transaction != _currentTransaction) throw new InvalidOperationException($"Transaction {transaction.TransactionId} is not current");
+
+            try
+            {
+                await SaveChangesAsync();
+                transaction.Commit();
+            }
+            catch
+            {
+                RollbackTransaction();
+                throw;
+            }
+            finally
+            {
+                if (_currentTransaction != null)
+                {
+                    _currentTransaction.Dispose();
+                    _currentTransaction = null;
+                }
+            }
+        }
+
+        public void RollbackTransaction()
+        {
+            try
+            {
+                _currentTransaction?.Rollback();
+            }
+            finally
+            {
+                if (_currentTransaction != null)
+                {
+                    _currentTransaction.Dispose();
+                    _currentTransaction = null;
+                }
+            }
+        }
+        #endregion
+    }
+}
